ac8f9953858cf26b8b0505255a079e2b1370dad1
Hollow Man <hollowman@opensuse.org>
2023-04-18 00:20:31 +0300

    Add code
    Signed-off-by: Hollow Man <hollowman@opensuse.org>

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,17 +1,12 @@
 #!/bin/sh
 set -e
 
-if [ -n "${GITHUB_WORKSPACE}" ] ; then
-  cd "${GITHUB_WORKSPACE}/${INPUT_WORKDIR}" || exit
-  git config --global --add safe.directory "${GITHUB_WORKSPACE}" || exit 1
-fi
-
 export REVIEWDOG_GITHUB_API_TOKEN="${INPUT_GITHUB_TOKEN}"
 
-misspell -locale="${INPUT_LOCALE}" . \
-  | reviewdog -efm="%f:%l:%c: %m" \
-      -name="linter-name (misspell)" \
-      -reporter="${INPUT_REPORTER:-github-pr-check}" \
+/convert.py "${INPUT_FILE}" \
+  | reviewdog -f="rdjson" \
+      -name="${INPUT_TOOL_NAME:-SARIF}" \
+      -reporter="${INPUT_REPORTER:-github-pr-review}" \
       -filter-mode="${INPUT_FILTER_MODE}" \
       -fail-on-error="${INPUT_FAIL_ON_ERROR}" \
       -level="${INPUT_LEVEL}" \
