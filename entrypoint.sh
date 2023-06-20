a46f74de5be4eef07422cfa754bef57b2e7a84ff
José Miguel Parrella <jparrel@microsoft.com>
2023-06-19 17:01:20 -0700

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -3,14 +3,11 @@ set -e
 
 export REVIEWDOG_GITHUB_API_TOKEN="${INPUT_GITHUB_TOKEN}"
 
-for line in `/convert.py "${INPUT_FILE}"`
-do
-  echo $line \
-  | reviewdog -f="rdjson" \
-      -name="${INPUT_TOOL_NAME:-SARIF}" \
-      -reporter="${INPUT_REPORTER:-github-pr-review}" \
-      -filter-mode="${INPUT_FILTER_MODE}" \
-      -fail-on-error="${INPUT_FAIL_ON_ERROR}" \
-      -level="${INPUT_LEVEL}" \
-      ${INPUT_REVIEWDOG_FLAGS}
-done
+/convert.py "${INPUT_FILE}" \
+| xargs -n1 reviewdog -f="rdjson" \
+    -name="${INPUT_TOOL_NAME:-SARIF}" \
+    -reporter="${INPUT_REPORTER:-github-pr-review}" \
+    -filter-mode="${INPUT_FILTER_MODE}" \
+    -fail-on-error="${INPUT_FAIL_ON_ERROR}" \
+    -level="${INPUT_LEVEL}" \
+    ${INPUT_REVIEWDOG_FLAGS}
