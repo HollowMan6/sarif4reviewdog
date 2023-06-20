9868528cf137dc7631da257e9dde54d2f2e654e5
José Miguel Parrella <jparrel@microsoft.com>
2023-06-19 17:22:51 -0700

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -3,6 +3,8 @@ set -e
 
 export REVIEWDOG_GITHUB_API_TOKEN="${INPUT_GITHUB_TOKEN}"
 
+./convert.py "${INPUT_FILE}" > /tmp/convert.json
+
 while read -r line
 do
   echo "$line" | reviewdog -f="rdjson" \
@@ -12,4 +14,4 @@ do
     -fail-on-error="${INPUT_FAIL_ON_ERROR}" \
     -level="${INPUT_LEVEL}" \
     ${INPUT_REVIEWDOG_FLAGS}
-done < ./convert.py "${INPUT_FILE}"
+done < /tmp/convert.json
